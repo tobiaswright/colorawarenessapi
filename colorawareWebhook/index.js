73dd6610af46a9ec0726906d663787df82f79f76
Tobias Wright <tobias.wright@gmail.com>
2017-10-11 15:05:05 -0400

    updated file

--- colorawareWebhook/index.js
+++ colorawareWebhook/index.js
@@ -1,19 +1,41 @@
+const request = require('request');
+
+const getData = (data) => {
+    const url = 'https://api.github.com/repos/'+data.repository.owner.name+'/'+data.repository.name+'/contents/'+data.commits[0].modified[0]
+    
+    const opts = {
+        url: url,
+        headers: {
+            'User-Agent': 'tobiaswright-colorawarenessapi',
+            'accept': 'application/vnd.github.VERSION.raw'
+        }
+    }
+
+    const promise = new Promise((resolve, reject) => {
+        request(opts, function (err, res, body) {
+
+            resolve(body)
+        });
+    });
+    
+    return promise
+};
+
+const formatData = (data) => {
+    const promise = new Promise((resolve, reject) => {
+        resolve(JSON.stringify(JSON.parse(data)))
+    });
+    
+    return promise
+}
+
 module.exports = function (context, data) {
-    context.log(data)
-    context.log('Webhook was triggered!');
-
-    // Check if we got first/last properties
-    // if('first' in data && 'last' in data) {
-    //     context.res = {
-    //         body: { greeting: 'Hello ' + data.first + ' ' + data.last + '!'}
-    //     };
-    // }
-    // else {
-    //     context.res = {
-    //         status: 400,
-    //         body: { error: 'Please pass first/last properties in the input object'}
-    //     };
-    // }
-
-    context.done();
+    // context.log(data);
+    // context.done();
+    getData(data)
+    .then(formatData)
+    .then(data => {
+        context.log(data);
+        context.done();
+    })
 }
